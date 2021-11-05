7224abf9c7bc74bbd1554c7988cd4313a995f88a
Vinicius Inacio <vminacio@paschoalotto.com.br>
2021-11-05 00:30:51 -0300

    paypal http service builder

--- src/data/datasource/http/pay-pal.http-datasource.ts
+++ src/data/datasource/http/pay-pal.http-datasource.ts
@@ -4,19 +4,20 @@ import { DataSourceError } from 'core/errors'
 import { ThrowsWhenUncaughtException, Cached, CacheKey } from 'core/middlewares'
 import { ENV_NAMES } from 'core/constants';
 import { PayPalApiBuilder } from 'core/services/http';
+import { auth } from './models/pay-pal.model';
 
 @Service()
 export class PayPalHttpDataSource {
 
   constructor(
-    private RestApiBuilder: PayPalApiBuilder,
+    private PayPalApiBuilder: PayPalApiBuilder,
     @Inject(ENV_NAMES.PAY_PAL) private envPaypal,
   ) { }
 
   @ThrowsWhenUncaughtException(DataSourceError)
-  async auth(): Promise<any> {
+  async auth(): Promise<auth> {
     const { CLIENT_SECRET, CLIENT_ID } = this.envPaypal;
-    return this.RestApiBuilder
+    return this.PayPalApiBuilder
       .post('/v1/oauth2/token')
       .headers({
         "Authorization": "Basic " + btoa(CLIENT_ID + ":" + CLIENT_SECRET)
@@ -31,7 +32,7 @@ export class PayPalHttpDataSource {
   @Cached()
   @ThrowsWhenUncaughtException(DataSourceError)
   async auth2(@CacheKey() { clientId, clientSecret }): Promise<any> {
-    return this.RestApiBuilder
+    return this.PayPalApiBuilder
       .post('/customers/oauth2/token')
       .params({
         grant_type: 'client_credentials',
